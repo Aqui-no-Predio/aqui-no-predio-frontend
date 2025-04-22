146489f4b6d55b155a5fb092da3d93f103a97f49
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-22 20:59:16 -0300

    style: Estilização na barra de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { LandingComponent } from "./shared/landing/landing.component";
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [LandingComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
