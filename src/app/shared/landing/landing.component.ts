6179eb8eb077b6a2438bd33c24a44f6c8bf0e500
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-23 20:27:49 -0300

    style: Estilização da landing page.

--- src/app/shared/landing/landing.component.ts
+++ src/app/shared/landing/landing.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardPostComponent } from "../components/card-post/card-post.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [CardPostComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
