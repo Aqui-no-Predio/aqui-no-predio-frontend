ee5503d7ae98c703b5a887f6c7e1dcd06c0c9de4
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-24 20:09:48 -0300

    style: Criação e estilização do footer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from "./shared/landing/landing.component";
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LandingComponent, NavbarComponent],
+  imports: [LandingComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
