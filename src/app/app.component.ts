ae233f0cc924616d53a100bef1680401051a773a
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-26 19:11:35 -0300

    feature: Routing funcionando e adicionado a barra de navegação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { PetsComponent } from "./shared/pets/pets.component";
 import { ServicesComponent } from "./shared/services/services.component";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingComponent, NavbarComponent, FooterComponent, PetsComponent, ServicesComponent],
+  imports: [LandingComponent, NavbarComponent, FooterComponent, PetsComponent, ServicesComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './shared/landing/landing.component';
+import { PetsComponent } from './shared/pets/pets.component';
+import { ServicesComponent } from './shared/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pets', component: PetsComponent },
+    { path: 'servicos', component: ServicesComponent },
+    { path: '**', component: LandingComponent },
+];
