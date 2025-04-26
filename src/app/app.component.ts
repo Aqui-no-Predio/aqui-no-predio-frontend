88e02570dc06eb5ca60a73ee1807534ad66e2ad2
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-26 19:46:20 -0300

    chore: Inclusão da logo do projeto.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { LandingComponent } from "./shared/landing/landing.component";
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
-import { PetsComponent } from "./shared/pets/pets.component";
-import { ServicesComponent } from "./shared/services/services.component";
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LandingComponent, NavbarComponent, FooterComponent, PetsComponent, ServicesComponent, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [ NavbarComponent, FooterComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
