a9dcea84e9109959b830b923d3f7cba26b7a92bc
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-26 22:23:02 -0300

    style: Criação e estilização da página de validação de chave de acesso e criação de post.

---
+++ src/app/shared/components/manager-login/manager-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-login',
+  imports: [],
+  templateUrl: './manager-login.component.html',
+  styleUrl: './manager-login.component.css'
+})
+export class ManagerLoginComponent {
+
+}
---
+++ src/app/shared/components/manager-post/manager-post.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-post',
+  imports: [],
+  templateUrl: './manager-post.component.html',
+  styleUrl: './manager-post.component.css'
+})
+export class ManagerPostComponent {
+
+}
