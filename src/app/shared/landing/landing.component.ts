5b92837d2809725c12e3a4454fd891da3002be94
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-26 22:29:14 -0300

    feature: Botões de redirecionamento funcionando.

--- src/app/shared/landing/landing.component.ts
+++ src/app/shared/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CardPostComponent } from "../components/card-post/card-post.component";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [CardPostComponent],
+  imports: [CardPostComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
