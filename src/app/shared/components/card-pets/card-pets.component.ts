d0d9df3ed86f0dc83bb934e35e17de3d003ff5ec
SayuriCristina <sayuri.cristinass@gmail.com>
2025-04-28 23:32:06 -0300

    feature: Modais para edição e deleção de pets.

--- src/app/shared/components/card-pets/card-pets.component.ts
+++ src/app/shared/components/card-pets/card-pets.component.ts
@@ -1,16 +1,25 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Pet } from '../../../models/pet.model';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-pets',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './card-pets.component.html',
   styleUrl: './card-pets.component.css'
 })
 export class CardPetsComponent {
   @Input() pet!: Pet;
-  
+  @ViewChild('editModalElement') editModalElement!: ElementRef;
+  @ViewChild('deleteModalElement') deleteModalElement!: ElementRef;
+
+  petForm!: FormGroup;
+  isEditModalOpen = false;
+  isDeleteModalOpen = false;
+
+  constructor(private fb: FormBuilder) { }
+
   getPetTypeClass(petType: String): string {
     switch (petType) {
       case 'Cachorro':
@@ -29,4 +38,44 @@ export class CardPetsComponent {
         return 'pet-image-other';
     }
   }
+  openEditModal(): void {
+    this.petForm = this.fb.group({
+      petName: [this.pet.petName, Validators.required],
+      petType: [this.pet.petType, Validators.required],
+      characteristics: [this.pet.characteristics, Validators.required],
+      person: [this.pet.person, Validators.required],
+      apartment: [this.pet.apartment, Validators.required]
+    });
+
+    this.isEditModalOpen = true;
+    document.body.classList.add('modal-open');
+  }
+
+  openDeleteModal(): void {
+    this.isDeleteModalOpen = true;
+    document.body.classList.add('modal-open');
+  }
+
+  closeEditModal(): void {
+    this.isEditModalOpen = false;
+    document.body.classList.remove('modal-open');
+  }
+
+  closeDeleteModal(): void {
+    this.isDeleteModalOpen = false;
+    document.body.classList.remove('modal-open');
+  }
+
+  savePet(): void {
+    if (this.petForm.valid) {
+      console.log('Método de edição ainda não implementado. Dados enviados:', this.petForm.value);
+      this.closeEditModal();
+    }
+  }
+
+  deletePet(): void {
+    console.log('Método de deleção ainda não implementado. ID do pet a ser deletado:', this.pet.id);
+    this.closeDeleteModal();
+  }
+
 }
