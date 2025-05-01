61eb24d3e1c2b64df28457597d17e8ba003b754a
SayuriCristina <sayuri.cristinass@gmail.com>
2025-05-01 00:07:12 -0300

    feature: Exibição dos componentes baseados em credencial de síndico.

--- src/app/core/services/access.service.ts
+++ src/app/core/services/access.service.ts
@@ -1,4 +1,3 @@
-// access.service.ts
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 
--- src/app/shared/manager/manager.component.ts
+++ src/app/shared/manager/manager.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { ManagerLoginComponent } from "../components/manager-login/manager-login.component";
 import { PostsComponent } from "../posts/posts.component";
+import { ManagerPostComponent } from "../components/manager-post/manager-post.component";
+import { Observable } from 'rxjs';
+import { AccessService } from '../../core/services/access.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-manager',
-  imports: [ManagerLoginComponent, PostsComponent],
+  imports: [ManagerLoginComponent, PostsComponent, ManagerPostComponent, CommonModule],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css'
 })
 export class ManagerComponent {
+  accessGranted$: Observable<boolean>;
 
+  constructor(private accessService: AccessService) {
+    this.accessGranted$ = this.accessService.accessGranted$;
+  }
+
+  showComponent: 'posts' | 'managerPost' | null = null;
 }
